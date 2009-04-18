9989e3a23aec9fd3bf44f4a1d1704047fc0519c2
Grissiom <chaos.proton@gmail.com>
2009-04-18 22:03:56 +0800

    TurnOffScreen: use system icon

--- TurnOffScreen/contents/code/main.py
+++ TurnOffScreen/contents/code/main.py
@@ -1,15 +1,16 @@
 # Written by Grissiom chaos.proton@gmail.com
 
 from PyQt4.QtCore import SIGNAL
-from PyQt4.QtGui import QGraphicsLinearLayout, QIcon
+from PyQt4.QtGui import QGraphicsLinearLayout, QWidget
 
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
-from PyKDE4.kdeui import KIcon, KGlobalSettings
+from PyKDE4.kdeui import KIcon, KGlobalSettings, KShortcutsDialog, KShortcutsEditor, \
+                KShortcutWidget
 
 import dbus
 
-import gui
+#import gui
 
 class TurnOffScreen(plasmascript.Applet):
 	def __init__(self, parent, args = None):
@@ -17,7 +18,7 @@ class TurnOffScreen(plasmascript.Applet):
 
 	def init(self):
                 #TODO: have a configuration interface to set keybroad shortcut
-		self.setHasConfigurationInterface(False)
+		#self.setHasConfigurationInterface(True)
 		self.setAspectRatioMode(Plasma.ConstrainedSquare)
                 self.setBackgroundHints(self.NoBackground)
 
@@ -25,11 +26,12 @@ class TurnOffScreen(plasmascript.Applet):
                 self.powerdevil = self.sessionBus.get_object('org.freedesktop.PowerManagement',
                                                              '/modules/powerdevil')
 
-                self.icon= Plasma.IconWidget(KIcon(':blank-screen.png'), '', self.applet)
+                self.icon= Plasma.IconWidget(KIcon('preferences-desktop-screensaver'), '', self.applet)
                 if KGlobalSettings.singleClick():
                         self.connect(self.icon, SIGNAL('clicked()'), self.turn_off_screen)
                 else:
                         self.connect(self.icon, SIGNAL('doubleClicked()'), self.turn_off_screen)
+                self.connect(self, SIGNAL('active()'), self.turn_off_screen)
 
                 self.layout = QGraphicsLinearLayout(self.applet)
                 self.layout.setContentsMargins(0, 0, 0, 0)
@@ -37,6 +39,10 @@ class TurnOffScreen(plasmascript.Applet):
                 self.layout.addItem(self.icon)
                 self.setLayout(self.layout)
                 self.resize(25, 25)
+
+        #def showConfigurationInterface(self):
+        #        self.con_short = KShortcutWidget(None)
+        #        self.con_short.show()
 
         def turn_off_screen(self):
                 self.powerdevil.turnOffScreen(dbus_interface='org.kde.PowerDevil')
